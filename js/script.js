efc666f1382b4030b2ef6127b14eb54e77447156
Buktop <vitekosipov@gmail.com>
2016-03-12 01:04:12 +0300

    Added files via upload

--- js/script.js
+++ js/script.js
@@ -6,27 +6,79 @@ function include(url) {
 include ("jquery-2.2.1.min.js");
 
 $(document).ready(function() {
+  $(".bottom").hide();
   var getfromjson = function(jsonurl, page, pageEnd) {
     $.getJSON(jsonurl, function(data) {
-      var jsonstring = '';
-      for (var num = page; num <= pageEnd; num++) {
-        jsonstring+='<div>'+data[num].id+': '+data[num].name+'<br>'+'</div>';
-      }
+      var jsonstring = '<table>';
+      for (var num = page; num < pageEnd; num++) {
+        jsonstring+='<tr>'+'<th>'+data[num].id+'</th>'+'<th>'+data[num].name+'</th>'+'</tr>';
+      };
+      jsonstring += '</table>';
       $(".content").html(jsonstring);
     })
   };
   $('#b1').on("click", function() {
+    $(".bottom").hide();
     $(".content").html("<div> HElloWorld! </div>");
   });
 
   $('#b2').on("click", function() {
+    $(".bottom").hide();
     getfromjson('JSON/data.json', 0, 4);
   });
 
   $('#b3').on("click", function() {
-    getfromjson('JSON/data2.json', 0, 17);
+    $(".bottom").hide();
+    getfromjson('JSON/data3.json', 0, 10);
   });
 
   $('#b4').on("click", function() {
+    $(".bottom").show();
+	  $.getJSON('JSON/data3.json', processData);
+    function processData(data) {
+		lastPage = data.length % 5;
+		numLastPage = data.length - lastPage;
+	}
+		page = 0 ;
+		pageEnd = 5;
+    getfromjson('JSON/data3.json',page, pageEnd);
+      $('.bottom').show();
+
+  $('#Next').on("click", function() {
+		if(pageEnd <= numLastPage) {
+			if(pageEnd == numLastPage) {
+		      page += 5;
+		      pageEnd += 5;
+		      empty = ' ';
+		      getfromjson('JSON/data3.json', page, numLastPage+lastPage);
+		  }
+		  else{
+        page = pageEnd;
+        pageEnd += 5;
+        getfromjson('JSON/data3.json', page, pageEnd);
+	    };
+    };
   });
-})
+
+	$('#Prvs').on("click", function(){
+		if (page > 4){
+      if (page == numLastPage) pageEnd -= lastPage
+      else pageEnd -= 5;
+      page -= 5;
+      getfromjson('JSON/data3.json', page, pageEnd);
+	   }
+  });
+
+  $('#First').on("click", function() {
+    page = 0;
+    pageEnd = 5;
+    getfromjson('JSON/data3.json', page, pageEnd);
+  });
+
+  $('#Last').on("click", function() {
+    page = numLastPage;
+    pageEnd = page + lastPage;
+    getfromjson('JSON/data3.json', numLastPage, numLastPage+lastPage);
+  })
+  });
+});
